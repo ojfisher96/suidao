fd5805e2b2db5cd4bdc7fcd23847ff24be508878
Daniel Phillips <dyproepsilon@gmail.com>
2014-01-02 19:53:47 +1100

    Implemented game loop and game timer class.

---
+++ src/game_loop.cpp
@@ -0,0 +1,41 @@
+#include "SDL/SDL.h"
+#include "suidao.hpp"
+
+namespace Suidao {
+
+Game::Run() {
+    Init();
+    for (;;) {
+        Input();
+        Network();
+        
+        _ts.nextTickStart();
+        Update();
+        _ts.nextTickEnd();
+        
+        Draw();
+    }
+}
+
+int Timestamp::get_tick() {
+    return _tick;
+}
+
+void Timestamp::nextTickStart() {
+    _tick++;
+    _lastTickStart = SDL_GetTicks();
+}
+
+void nextTickEnd() {
+    _lastTickEnd = SDL_GetTicks();
+}
+
+unsigned long long timeSinceLastTickStart() {
+    return SDL_GetTicks() - _lastTickStart;
+}
+
+unsigned long long timeSinceLastTickEnd() {
+    return SDL_GetTicks() - _lastTickEnd;
+}
+
+}
