0bd0bcbbdfd9a0b2871b3ca1a6920ba458ddef09
Daniel Phillips <dyproepsilon@gmail.com>
2014-01-02 19:52:46 +1100

    Added definition for game. Should be at least sufficient for now.

---
+++ src/suidao.hpp
@@ -0,0 +1,41 @@
+#ifndef SUIDAO_HPP
+#define SUIDAO_HPP
+
+#include "map.hpp"
+
+namespace Suidao {
+
+class Game {
+  private:
+    Timestamp _ts;
+    
+    Map map;
+
+    void Init();
+    void Input();
+    void Network();
+    void Update();
+    void Draw();
+  public:
+    void Run();
+    Game();
+};
+
+class Timestamp {
+  private:
+    int _tick;
+    unsigned long long _lastTickStart;
+    unsigned long long _lastTickEnd;
+  public:
+    int get_tick();
+    void nextTickStart();
+    void nextTickEnd();
+    // Both in milliseconds
+    unsigned long long timeSinceLastTickStart(); 
+    unsigned long long timeSinceLastTickEnd();
+    Timestamp();
+}
+
+}
+
+#endif // SUIDAO_HPP
