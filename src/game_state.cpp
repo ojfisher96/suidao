40d091e8cbbd627b32bc89667df4dd3319639d4b
Daniel Phillips <dyproepsilon@gmail.com>
2014-08-15 08:32:23 +1000

    lua api test

--- src/game_state.cpp
+++ src/game_state.cpp
@@ -1,6 +1,11 @@
 #include "game_state.hpp"
 #include "coordinate.hpp"
 
+#include "lua5.2/lua.hpp"
+#include "lua_api.hpp"
+
+#include <cstdio>
+
 namespace Suidao {
 
 GameState::GameState() {}
@@ -10,8 +15,24 @@ GameState::GameState(const Map& m) {
 }
 
 bool GameState::ProcessCommand(Command c) {
+    
+    lua_State *ls = luaL_newstate();
+    InitialiseLuaAPI(&map, ls);
+
     // Stub for test of cutting
     map.GetColumn(Coord2<int>(1,9)).MakeCut(2,1);
+
+    if (luaL_dostring(ls, "function foo () return Map.GetSegment(0,0,0).top end")) {
+        printf("bleh\n");
+        fflush(stdout);
+    } else {
+        lua_getglobal(ls, "foo");
+        lua_call(ls, 0, 1);
+        printf("Result: %d\n", lua_tointeger(ls, -1));
+    }
+    lua_close(ls);
+    
+    
     return true;
 }
 
